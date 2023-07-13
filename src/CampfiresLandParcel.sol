383c83faab1900867e1d3bf14dd8e7f96655bffa
Carlo Miguel Dy <carlomigueldy@gmail.com>
2023-07-13 13:19:51 +0800

    Init erc721 land parcel contract

--- src/CampfiresLandParcel.sol
+++ src/CampfiresLandParcel.sol
@@ -2,7 +2,18 @@
 pragma solidity ^0.8.19;
 
 import "erc721a/contracts/ERC721A.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
 
-contract CampfiresLandParcel is ERC721A {
-    constructor() ERC721A("Campfires Land Parcel", "CLP") {}
+/// @title CampfiresLandParcel
+/// @author Carlo Miguel Dy
+contract CampfiresLandParcel is ERC721A, AccessControl {
+    constructor() ERC721A("Campfires Land Parcel", "CLP") {
+        _setupRole(DEFAULT_ADMIN_ROLE, _msgSender());
+    }
+
+    function supportsInterface(
+        bytes4 interfaceId
+    ) public view virtual override(AccessControl, ERC721A) returns (bool) {
+        return super.supportsInterface(interfaceId);
+    }
 }
