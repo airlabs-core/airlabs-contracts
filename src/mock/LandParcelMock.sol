f46c213c3103f0095476ba29a526c404649ccb9f
Carlo Miguel Dy <carlomigueldy@gmail.com>
2023-08-04 16:41:14 +0800

    Update deploy params adding staking rewards contract

--- src/mock/LandParcelMock.sol
+++ src/mock/LandParcelMock.sol
@@ -7,8 +7,16 @@ contract LandParcelMock is LandParcel {
     constructor(
         address _experiencePoint,
         address _erc6551Implementation,
-        address _erc6551Registry
-    ) LandParcel(_experiencePoint, _erc6551Implementation, _erc6551Registry) {}
+        address _erc6551Registry,
+        address _stakingRewards
+    )
+        LandParcel(
+            _experiencePoint,
+            _erc6551Implementation,
+            _erc6551Registry,
+            _stakingRewards
+        )
+    {}
 
     function mint(address to, uint256 quantity) external {
         _mint(to, quantity);
