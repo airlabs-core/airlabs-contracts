26b4a02f1bdd4cc32d2bc43b58767a6915f7b679
Carlo Miguel Dy <carlomigueldy@gmail.com>
2023-08-07 10:11:18 +0800

    Add withdraw function and balanceOf query function to staking rewards interface

--- src/interfaces/IStakingRewards.sol
+++ src/interfaces/IStakingRewards.sol
@@ -2,5 +2,9 @@
 pragma solidity ^0.8.19;
 
 interface IStakingRewards {
+    function balanceOf(address _account) external returns (uint);
+
     function stake(uint256 _amount) external;
+
+    function withdraw(uint256 _amount) external;
 }
